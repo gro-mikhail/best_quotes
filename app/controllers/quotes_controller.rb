cd44d397a262e8703643c6234ab9865b283d7ceb
mikhail <hramyka.mikhail>
2023-01-03 23:31:26 +0300

    second part done

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class QuotesController < Rulers::Controller
+  def a_quote
+    "There is nothing either good or bad but thinking makes it so.\n<pre>\n#{env}\n</pre>"
+  end
+
+  def exception
+    raise "It's a bad one!"
+  end
+end
